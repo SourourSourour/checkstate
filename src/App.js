384b34319c8764df1699f489f0a4cd59c8f0bf75
SourourSourour <issaoui.sourour22@gmail.com>
2022-05-31 14:02:57 +0100

    state

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import React , {Component}from "react";
+import Profile from './Profile/Profile';
+
+class App extends Component {
+  state={
+    person: {
+      fullName: "Sourour Issaoui",
+      bio: "student at GO MY CODE",
+      profession: "web developper",
+      imgSrc:"./photo.jpg"
+      
+    },
+    shows: false,
+    timer:0
+  }
+  handleClick=()=>this.setState({shows:!this.state.shows})
+  componentDidMount() {
+    setInterval(() => {
+        this.setState({ timer: this.state.timer + 1 })
+    }, 1000)   
+}
+  render(){
+  return (
+    <div className="App">
+  <button className='button' onClick={this.handleClick}> {this.state.shows ? "hide":"show"}</button>      
+{
+  this.state.shows?
+  <Profile pro={this.state.person}/>
+
+ :<h4 className='txt'>click to show  your profile </h4>
+}
+<h1 className="cnt">Timer: {this.state.timer}{" "}seconds</h1>
+
+    </div>
+  );
+}
+}
+
+export default App;
